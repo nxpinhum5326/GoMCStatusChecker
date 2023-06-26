8b24a4107e2092e2822c3ea694336b2c4c7ff0d7
scher470 <120848089+nxpinhum5326@users.noreply.github.com>
2023-06-26 15:36:41 +0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	mcrcon "github.com/Kelwing/mc-rcon"
+)
+
+func main() {
+	conn := new(mcrcon.MCConn)
+	err := conn.Open("xxx.xxx.x.xx:PORT", "rconPassword")
+	if err != nil {
+		log.Println("Connection failed!", err)
+	}
+	defer conn.Close()
+
+	err = conn.Authenticate()
+	if err != nil {
+		log.Println("Authorization failed!", err)
+	}
+
+	result, err := conn.SendCommand("list")
+	if err != nil {
+		log.Println("Command not found or broken!", err)
+	}
+
+	log.Println(result)
+}
